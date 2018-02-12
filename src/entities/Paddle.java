0f7fdccb0442f19d02abe2d87cdabcdbd854419b
Christos Cunning <christoscunn@gmail.com>
2018-02-12 16:40:58 -0500

    Tried some stuff, draw is kinda working

--- src/entities/Paddle.java
+++ src/entities/Paddle.java
@@ -1,12 +1,20 @@
 package entities;
 
-import acm.graphics.GRectangle;
+import java.awt.Graphics;
 
-public class Paddle extends GRectangle {
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+import acm.graphics.GRectangle;
+import acm.program.GraphicsProgram;
+import pong.Main;
+
+public class Paddle extends GRect {
 	
-	private float x,y;
-	private float spd;
-	private float size; // Size: = how wide paddle is, height is twice this
+	public static final double SPD = 5;
+	
+	private double x,y;
+	private double width, height; // Size: = how wide paddle is, height is twice this
+	private double dir; // in radians
 	
 	/** Constructor for Paddle Object
 	 *  this constructor calls the super Constructor GRectangle with all of the following parameters.
@@ -16,18 +24,41 @@ public class Paddle extends GRectangle {
 	 * @param w starting width of the paddle
 	 * @param h starting height of the paddle
 	 */
-	public Paddle (float x, float y, float spd, float size) {
+	public Paddle (double w, double h, double x, double y) {
+		super(w,h);
+		width = w;
+		height = h;
 		this.x = x;
 		this.y = y;
-		this.spd = spd;
-		this.size = size;
+		dir = Main.getRandNumBetween(0, Math.PI*2);
 	}
 	
-	public void draw() {
-		
+	/** Constructor for Paddle Object
+	 *  this constructor calls the super Constructor GRectangle with all of the following parameters. If x,y are not specified, paddle starts at 0,0
+	 * 
+	 * @param x starting x is 0
+	 * @param y starting y is 0
+	 * @param w starting width of the paddle
+	 * @param h starting height of the paddle
+	 */
+	public Paddle (double w, double h) {
+		super(w,h);
+		width = w;
+		height = h;
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public void draw(GraphicsProgram g) {
+		g.add(this,x,y);
 	}
 	
 	public void move() {
+		x += SPD * Math.cos(dir);
+		y += SPD * Math.sin(dir);
+	}
+	
+	public void init (GraphicsProgram g) {
 		
 	}
 	
--- src/pong/Main.java
+++ src/pong/Main.java
@@ -1,25 +1,65 @@
 package pong;
 
-
 import entities.*;
-import processing.core.PApplet;
+import acm.graphics.GObject;
+import acm.program.GraphicsProgram;
 
-public class Main extends PApplet {
+public class Main extends GraphicsProgram {
 	
+	// window stuff
+	public static final int SCREEN_W = 600;
+	public static final int SCREEN_H = 400;
+	private static final int PAUSE = 10; // or whatever interval you like
+	
+	// paddle stuff
 	public static final float STARTING_SPEED = 5;
 	public static final float STARTING_SIZE = 10;
 	
-	Paddle p1 = new Paddle(10,10,101,10);
+	private boolean gameRunning = true;
+	
+	Paddle p1 = new Paddle(50,50);
 	Paddle p2;
 	Ball ball;
 	
 	public void run () {
+		/*
+		this.resize(SCREEN_W,SCREEN_H);
+		pause(PAUSE);
+		*/
+		while(true) {
+			p1.move();
+			p1.draw(this);
+			
+			println("runnin");
+			int input = readInt();
+			if(input == 1) {
+				gameRunning = false;
+				break;
+			}
+		}
+	}
+	
+	public void keyPressed () {
+		
+		if(getKeyCode() == 27) {
+			
+		}
 		
 	}
 	
-	/*
-	public static void main(String[] args) {
-		PApplet.main("pong.Main");
+	public void init () {
+		setSize(SCREEN_W,SCREEN_H);
 	}
-	*/
+	
+	
+	/** Method to get random number between 0 and n (inclusive). To get position of vertices of randomly generated asteroid.
+	 * 
+	 * @param n upper limit of random range
+	 * @return returns a random int between 0 and n.
+	 */
+	public static double getRandNumBetween (double nzero, double n) {
+		double ranr = nzero + ( (Math.random() * n ) + 1);
+		return ranr;
+		
+	}
 }
